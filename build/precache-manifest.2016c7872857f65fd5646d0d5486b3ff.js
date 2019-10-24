0e83109bc7999acb2e88ebbc872671084448af97
Igor <igor.ogerchuk@gmail.com>
2019-10-25 01:48:56 +0300

    build folder added

---
+++ build/precache-manifest.2016c7872857f65fd5646d0d5486b3ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0244115463a9db49fc6f2edcf1b9810b",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "7256f1909d6d59c7a4f2",
+    "url": "/goit-react-hw-02-reader/static/css/main.0e021f61.chunk.css"
+  },
+  {
+    "revision": "4a1d784fac28479a55ef",
+    "url": "/goit-react-hw-02-reader/static/js/2.e12b5917.chunk.js"
+  },
+  {
+    "revision": "7256f1909d6d59c7a4f2",
+    "url": "/goit-react-hw-02-reader/static/js/main.222b83f3.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
