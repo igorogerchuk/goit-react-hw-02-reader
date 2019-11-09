b7e77fc1392b81bbf206d5e2510dc9a472222521
Igor <igor.ogerchuk@gmail.com>
2019-11-09 17:22:50 +0200

    added build

--- build/precache-manifest.f4a2bde20fca2572bd0713c0f6bbffb0.js
+++ build/precache-manifest.f4a2bde20fca2572bd0713c0f6bbffb0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0244115463a9db49fc6f2edcf1b9810b",
+    "revision": "32bde06798a0fe9c2e35a2ca31697e56",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "7256f1909d6d59c7a4f2",
-    "url": "/goit-react-hw-02-reader/static/css/main.0e021f61.chunk.css"
+    "revision": "ef7fe6bd62c6eba84a79",
+    "url": "/goit-react-hw-02-reader/static/css/main.b8a5dfe5.chunk.css"
   },
   {
     "revision": "4a1d784fac28479a55ef",
     "url": "/goit-react-hw-02-reader/static/js/2.e12b5917.chunk.js"
   },
   {
-    "revision": "7256f1909d6d59c7a4f2",
-    "url": "/goit-react-hw-02-reader/static/js/main.222b83f3.chunk.js"
+    "revision": "ef7fe6bd62c6eba84a79",
+    "url": "/goit-react-hw-02-reader/static/js/main.ac907cb0.chunk.js"
   },
   {
     "revision": "0da2320c246f0dcbed6e",
