b7f02e10088ba81d55ef7c98e45af3f84285840e
Igor <igor.ogerchuk@gmail.com>
2019-11-13 01:58:58 +0200

    Updates

--- precache-manifest.a5cf444d7bc3f47ae0aa9dc578955a8a.js
+++ precache-manifest.a5cf444d7bc3f47ae0aa9dc578955a8a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32bde06798a0fe9c2e35a2ca31697e56",
+    "revision": "67f5ff30ee7ab18f947230c95619cce8",
     "url": "/goit-react-hw-02-reader/index.html"
   },
   {
-    "revision": "ef7fe6bd62c6eba84a79",
-    "url": "/goit-react-hw-02-reader/static/css/main.b8a5dfe5.chunk.css"
+    "revision": "07562ad28bae686a08ac",
+    "url": "/goit-react-hw-02-reader/static/css/main.c6f12341.chunk.css"
   },
   {
     "revision": "4a1d784fac28479a55ef",
     "url": "/goit-react-hw-02-reader/static/js/2.e12b5917.chunk.js"
   },
   {
-    "revision": "ef7fe6bd62c6eba84a79",
-    "url": "/goit-react-hw-02-reader/static/js/main.ac907cb0.chunk.js"
+    "revision": "07562ad28bae686a08ac",
+    "url": "/goit-react-hw-02-reader/static/js/main.604c9359.chunk.js"
   },
   {
     "revision": "0da2320c246f0dcbed6e",
